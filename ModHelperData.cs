a0b01cca1d003457fb8dd1ab63e7e1851f2a5c5d
Mattcy <140085165+Mattcy1@users.noreply.github.com>
2023-10-31 17:18:49 +0100

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -4,7 +4,7 @@ public static class ModHelperData
 {
     public const string Name = "Halloween";
     public const string Description = "Halloween Is Right Around The Corner With The Halloween Update in Btd6 i fell like it isnt enough which is why im adding this mod Which Add A Tower With A Fourth Path?! Require Path plus plus And a new gamemode themed around halloween with custom bloon?! Credit To @PoppingProductionsYT for art idea etc";
-    public const string Version = "Halloween 1.0";
+    public const string Version = "1.0";
     public const string RepoOwner = "Mattcy1";
     public const string RepoName = "Halloween";
     public const string WorksOnVersion = "39.2";
